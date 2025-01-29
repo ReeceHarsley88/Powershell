23e2d7ef985b4e8069eb8f88fef9fe02f804fd98
Reece Harsley <67327922+ReeceHarsley88@users.noreply.github.com>
2025-01-29 17:18:09 +0000

    Update Disable Copilot Canvas Search,Sort,Filter.ps1

--- Disable Copilot Canvas Search,Sort,Filter.ps1
+++ Disable Copilot Canvas Search,Sort,Filter.ps1
@@ -2,3 +2,5 @@
 Set-AdminPowerAppEnvironmentCopilotSettings -EnvironmentName <Environment ID> -CanvasAppGalleryFilterCopilotEnabled $false
 #for an App 
 Set-PowerAppSettings -AppName 'AppName' -DraftingCopilotEnabled $false
+
+https://learn.microsoft.com/en-us/power-apps/user/smartgrid
